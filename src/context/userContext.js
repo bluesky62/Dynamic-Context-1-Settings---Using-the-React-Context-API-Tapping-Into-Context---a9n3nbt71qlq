5498fbdbcbd4636be35b36f5fba1106ec33d3ca2
Akash Ingale <92642182+bluesky62@users.noreply.github.com>
2022-10-31 13:25:50 +0530

    Update userContext.js

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -5,15 +5,11 @@ const UserContext = createContext()
 
 const Wrapper = (props) => {
 
-<<<<<<< HEAD
-    const greeting = "Hello" + {greeting}
-=======
     // const greeting = "Hello"
     const [greeting, setGreet] = useState('Hello');
     const changeGreeting = (e) => {
         setGreet(e.target.value);
     }
->>>>>>> 33ea35ace746b17f44fe24132ae99a2cea5202db
   
     return (
         <UserContext.Provider value={{ greeting, changeGreeting }}>
