f60196224c8ee67595cafaf511b0cec5a82898ea
akash <akashingale62@gmail.com>
2022-10-31 13:14:28 +0530

    adf

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,16 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import { useContext } from "react"
 import { UserContext } from "../context/userContext"
 
 export const Settings = () => {
 
     // to be implemented in context
-    const { changeGreeting } = useContext(UserContext)
-
+    
+    const { changeGreeting } = useContext(UserContext);
+   
     return (
         <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
             <h4>Settings</h4>
-            <input type={'text'} />
+            <input type={'text'} onChange={(e)=> changeGreeting(e)} />
         </div>
     )
 }
